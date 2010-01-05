be176721572d31973251694b660c7d0ee66b4668
Thomas Baum <thomas.baum@atns.de>
2010-01-05 16:15:10 +0100

    security logging

--- common-security/src/main/java/de/atns/common/security/AuthenticateFilter.java
+++ common-security/src/main/java/de/atns/common/security/AuthenticateFilter.java
@@ -2,6 +2,8 @@ package de.atns.common.security;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +21,7 @@ public class AuthenticateFilter implements Filter {
 
     static final String SESSION_USER = "_SECURITY_USER";
     static final String SESSION_UUID = "_SECURITY_UUID";
+    private static final Log LOG = LogFactory.getLog(AuthenticateFilter.class);
     private final SecurityService securityService;
 
 // --------------------------- CONSTRUCTORS ---------------------------
@@ -43,13 +46,15 @@ public class AuthenticateFilter implements Filter {
         final String path = req.getRequestURI();
 
         if (path.contains("_security_login")) {
-            final SecurityService.SecurityUserToken token = securityService.login(req.getParameter("login"),
-                    req.getParameter("password"));
+            LOG.debug("perfom login");
+            final SecurityUserToken token = securityService.login(req.getParameter("login"), req.getParameter("password"));
+            LOG.debug("token =" + token.getUuid());
 
             req.getSession(true).setAttribute(SESSION_USER, token.getUser());
             req.getSession(true).setAttribute(SESSION_UUID, token.getUuid().toString());
             ((HttpServletResponse) response).setHeader("X-Authorization", token.getUuid().toString());
         } else if (path.contains("_security_logout")) {
+            LOG.debug("perfom logout");
             final HttpSession session = req.getSession(false);
             if (session != null) {
                 session.removeAttribute(SESSION_USER);
