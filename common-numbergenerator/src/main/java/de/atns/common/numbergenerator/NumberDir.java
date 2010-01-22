5951266a33a338c7da49626c3b617d8a419286eb
Michael Wolter <mwolter@mbp.lan>
2010-01-22 12:18:11 +0100

    ContextListener ConfigurationName Annotation added

--- common-numbergenerator/src/main/java/de/atns/common/numbergenerator/NumberDir.java
+++ common-numbergenerator/src/main/java/de/atns/common/numbergenerator/NumberDir.java
@@ -1,6 +1,7 @@
 package de.atns.common.numbergenerator;
 
 import com.google.inject.BindingAnnotation;
+import de.atns.common.config.ConfigurationName;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -11,5 +12,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @BindingAnnotation
 @Retention(RUNTIME)
 @Target(PARAMETER)
+@ConfigurationName("ordernumber-dir")
 public @interface NumberDir {
 }
