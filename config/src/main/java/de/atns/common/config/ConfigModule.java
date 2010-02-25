7111454dc62ee69945985b65b596cc61e8583eaa
Thomas Baum <thomas.baum@atns.de>
2010-02-25 11:42:22 +0100

    automatic configuration

--- config/src/main/java/de/atns/common/config/ConfigModule.java
+++ config/src/main/java/de/atns/common/config/ConfigModule.java
@@ -3,6 +3,7 @@ package de.atns.common.config;
 import com.google.inject.AbstractModule;
 import com.google.inject.MembersInjector;
 import com.google.inject.TypeLiteral;
+import com.google.inject.matcher.Matchers;
 import com.google.inject.spi.TypeEncounter;
 import com.google.inject.spi.TypeListener;
 import com.google.inject.util.Providers;
@@ -16,7 +17,7 @@ import java.lang.reflect.Field;
  * @author tbaum
  * @since 22.01.2010
  */
-public abstract class ConfigModule extends AbstractModule {
+public class ConfigModule extends AbstractModule {
 // ------------------------------ FIELDS ------------------------------
 
     private static final Log LOG = LogFactory.getLog(ConfigModule.class);
@@ -25,12 +26,41 @@ public abstract class ConfigModule extends AbstractModule {
 // -------------------------- OTHER METHODS --------------------------
 
     @Override protected final void configure() {
+        bindListener(Matchers.any(), new TypeListener() {
+            @Override public <I> void hear(final TypeLiteral<I> type, final TypeEncounter<I> encounter) {
+                final Class<? super I> rawType = type.getRawType();
+                for (final Field field : rawType.getDeclaredFields()) {
+                    for (Annotation annotation : field.getAnnotations()) {
+                        final ConfigurationName cn = annotation.annotationType().getAnnotation(ConfigurationName.class);
+
+                        if (cn != null) {
+                            field.setAccessible(true);
+                            final String value = ConfigModule.this.value(cn.value());
+                            LOG.info("bind " + rawType.getName() + "#" + field.getName() + " to " + value);
+                            encounter.register(new MembersInjector<I>() {
+                                @Override public void injectMembers(final I instance) {
+                                    try {
+                                        field.set(instance, value);
+                                    } catch (IllegalAccessException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+        });
+
         configureConfig();
-//        bindListener(Matchers.any(), new ConfigAnnotationTypeListener());
     }
 
-    protected abstract void configureConfig();
+    protected String value(final String name) {
+        return config.get(name);
+    }
 
+    protected void configureConfig() {
+    }
 
     protected void configure(final Class<? extends Annotation> annotation) {
         String name = annotation.getAnnotation(ConfigurationName.class).value();
@@ -41,53 +71,6 @@ public abstract class ConfigModule extends AbstractModule {
         } else {
             LOG.warn("configuration value for '" + config.getPropertyName(name) + "' is null");
             bind(String.class).annotatedWith(annotation).toProvider(Providers.of((String) null));
-        }
-    }
-
-    protected String value(final String name) {
-        return config.get(name);
-    }
-
-// -------------------------- INNER CLASSES --------------------------
-
-    class ConfigAnnotationTypeListener implements TypeListener {
-        public <T> void hear(TypeLiteral<T> typeLiteral, TypeEncounter<T> typeEncounter) {
-            System.err.println("test " + typeLiteral);
-            for (Field field : typeLiteral.getRawType().getDeclaredFields()) {
-                if (field.getType() == String.class) {
-                    for (Annotation annotation : field.getAnnotations()) {
-                        System.err.println(" ." + annotation);
-
-                        ConfigurationName ca = annotation.getClass().getAnnotation(ConfigurationName.class);
-                        if (ca != null) {
-                            System.err.println(" >" + ca);
-
-                            String name = ca.value();
-                            typeEncounter.register(new ConfigAnnotationMembersInjector<T>(field, value(name)));
-                            LOG.debug("set value for " + field + " to #" + name + "# = " + value(name));
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    class ConfigAnnotationMembersInjector<T> implements MembersInjector<T> {
-        private final Field field;
-        private final String value;
-
-        ConfigAnnotationMembersInjector(Field field, String value) {
-            this.field = field;
-            this.value = value;
-            field.setAccessible(true);
-        }
-
-        public void injectMembers(T t) {
-            try {
-                field.set(t, value);
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 }                                                   
