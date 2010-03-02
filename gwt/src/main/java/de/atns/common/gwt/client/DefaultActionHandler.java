b1e9141ab5b890e55cf08a50f0b0d6354f2ef4e1
Thomas Baum <thomas.baum@atns.de>
2010-03-02 15:41:39 +0100

    default action handler

---
+++ gwt/src/main/java/de/atns/common/gwt/client/DefaultActionHandler.java
@@ -0,0 +1,26 @@
+package de.atns.common.gwt.client;
+
+import net.customware.gwt.dispatch.server.ActionHandler;
+import net.customware.gwt.dispatch.server.ExecutionContext;
+import net.customware.gwt.dispatch.shared.Action;
+import net.customware.gwt.dispatch.shared.ActionException;
+import net.customware.gwt.dispatch.shared.Result;
+
+/**
+ * @author tbaum
+ * @since 12.02.2010
+ */
+public abstract class DefaultActionHandler<A extends Action<R>, R extends Result>
+        implements ActionHandler<A, R> {
+// ------------------------ INTERFACE METHODS ------------------------
+
+
+// --------------------- Interface ActionHandler ---------------------
+
+    @Override    //TODO  @Transactional
+    public abstract R execute(final A action, final ExecutionContext executionContext) throws ActionException;
+
+    @Override
+    public void rollback(final A action, final R result, final ExecutionContext context) {
+    }
+}
