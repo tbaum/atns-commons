f7e4d8c66e11f73c35bd5b75d9f3665e89d6ac69
Thomas Baum <thomas.baum@atns.de>
2010-05-11 19:16:01 +0200

    fix SimpleResourceLoader

---
+++ dao-mail/src/main/java/de/atns/common/util/SimpleResourceLoader.java
@@ -0,0 +1,40 @@
+package de.atns.common.util;
+
+import org.apache.commons.collections.ExtendedProperties;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.runtime.resource.Resource;
+import org.apache.velocity.runtime.resource.loader.ResourceLoader;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+public class SimpleResourceLoader extends ResourceLoader {
+// -------------------------- OTHER METHODS --------------------------
+
+    @Override
+    public long getLastModified(final Resource resource) {
+        return 0;
+    }
+
+    @Override
+    public InputStream getResourceStream(final String s) throws ResourceNotFoundException {
+        byte[] data;
+        try {
+            data = s.getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            data = s.getBytes();
+        }
+        return new ByteArrayInputStream(data);
+    }
+
+    @Override
+    public void init(final ExtendedProperties extendedProperties) {
+        //
+    }
+
+    @Override
+    public boolean isSourceModified(final Resource resource) {
+        return false;
+    }
+}
