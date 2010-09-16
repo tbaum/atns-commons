2857b8cd007a169dd85576606567f469e94cfb4e
Thomas Baum <thomas.baum@atns.de>
2010-09-17 00:30:23 +0200

    IsSerializable --> Serializable

--- gwt/src/main/java/de/atns/common/gwt/client/model/ListPresentation.java
+++ gwt/src/main/java/de/atns/common/gwt/client/model/ListPresentation.java
@@ -3,6 +3,7 @@ package de.atns.common.gwt.client.model;
 import com.google.gwt.user.client.rpc.IsSerializable;
 import net.customware.gwt.dispatch.shared.Result;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -10,7 +11,7 @@ import java.util.Collection;
  * @author tbaum
  * @since 12.02.2010
  */
-public class ListPresentation<E extends IsSerializable> implements Result, IsSerializable {
+public class ListPresentation<E extends Serializable> implements Result, IsSerializable {
 // ------------------------------ FIELDS ------------------------------
 
     private ArrayList<E> list;
