c50f15662228c26c1703a0c94c2bb23091ad07e2
michaelw <michael.wolter@atns.de>
2010-09-22 19:36:13 +0200

    gwt crud module (loadList)

---
+++ gwt-crud/src/main/java/de/atns/common/crud/client/event/LoadListEvent.java
@@ -0,0 +1,57 @@
+package de.atns.common.crud.client.event;
+
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.rpc.IsSerializable;
+import de.atns.common.gwt.client.ErrorWidgetDisplay;
+import de.atns.common.gwt.client.model.ListPresentation;
+import de.atns.common.security.client.Callback;
+import de.atns.common.security.client.DefaultCallback;
+import net.customware.gwt.dispatch.client.DispatchAsync;
+import net.customware.gwt.presenter.client.EventBus;
+
+import static de.atns.common.security.client.DefaultCallback.callback;
+
+
+/**
+ * @author mwolter
+ * @since 22.09.2010 18:17:01
+ */
+public class LoadListEvent<E extends IsSerializable> extends GwtEvent<LoadListEventHandler<E>> {
+// ------------------------------ FIELDS ------------------------------
+
+    private final ListPresentation<E> result;
+    private Type<LoadListEventHandler<E>> TYPE;
+
+// -------------------------- STATIC METHODS --------------------------
+
+    public static <T extends IsSerializable> DefaultCallback<ListPresentation<T>> eventCallback(
+            final DispatchAsync dispatcher,
+            final EventBus eventBus,
+            final ErrorWidgetDisplay display,
+            final GwtEvent.Type<LoadListEventHandler<T>> type) {
+
+        return callback(dispatcher, eventBus, display, new Callback<ListPresentation<T>>() {
+            @Override public void callback(final ListPresentation<T> result11) {
+                eventBus.fireEvent(new LoadListEvent<T>(result11, type));
+            }
+        });
+
+    }
+
+// --------------------------- CONSTRUCTORS ---------------------------
+
+    private LoadListEvent(final ListPresentation<E> result, final Type<LoadListEventHandler<E>> type) {
+        this.result = result;
+        TYPE = type;
+    }
+
+// -------------------------- OTHER METHODS --------------------------
+
+    @Override protected void dispatch(final LoadListEventHandler<E> loadListEventHandler) {
+        loadListEventHandler.onLoad(result);
+    }
+
+    @Override public Type<LoadListEventHandler<E>> getAssociatedType() {
+        return TYPE;
+    }
+}
---
+++ gwt-crud/src/main/java/de/atns/common/crud/client/event/LoadListEventHandler.java
@@ -0,0 +1,15 @@
+package de.atns.common.crud.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.user.client.rpc.IsSerializable;
+import de.atns.common.gwt.client.model.ListPresentation;
+
+/**
+ * @author mwolter
+ * @since 22.09.2010 18:16:46
+ */
+public interface LoadListEventHandler<E extends IsSerializable> extends EventHandler {
+// -------------------------- OTHER METHODS --------------------------
+
+    void onLoad(ListPresentation<E> event);
+}
