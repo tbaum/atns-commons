ed9f4dff311b3bfc7988d64e3aaa330bf88ee3dd
michaelw <michael.wolter@atns.de>
2010-09-23 13:20:39 +0200

    Serializable bei Result hinzugefügt

--- gwt-dispatch/src/main/java/net/customware/gwt/dispatch/shared/Result.java
+++ gwt-dispatch/src/main/java/net/customware/gwt/dispatch/shared/Result.java
@@ -2,10 +2,12 @@ package net.customware.gwt.dispatch.shared;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+import java.io.Serializable;
+
 /**
  * A marker interface for {@link Action} results.
  *
  * @author David Peterson
  */
-public interface Result extends IsSerializable {
+public interface Result extends IsSerializable, Serializable {
 }
