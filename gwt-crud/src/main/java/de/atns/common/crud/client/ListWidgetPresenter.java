3f8c430f7e8ba1c3995f92b04d0a28440dc4eca0
Michael Wolter <michael.wolter@atns.de>
2010-10-05 19:30:40 +0200

    abstract PagePresenter, listevent

--- gwt-crud/src/main/java/de/atns/common/crud/client/ListWidgetPresenter.java
+++ gwt-crud/src/main/java/de/atns/common/crud/client/ListWidgetPresenter.java
@@ -1,6 +1,9 @@
 package de.atns.common.crud.client;
 
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.rpc.IsSerializable;
 import com.google.inject.Inject;
+import de.atns.common.crud.client.event.LoadListEventHandler;
 import de.atns.common.gwt.client.DefaultWidgetPresenter;
 import de.atns.common.gwt.client.ListPresenter;
 
@@ -29,9 +32,11 @@ public abstract class ListWidgetPresenter<D extends ListWidgetDisplay> extends D
     @Override protected void onBind() {
         super.onBind();
 
-        pagePresenter.bind(this);
+        pagePresenter.bind(this, _listEvent());
         display.setPagePresenter(pagePresenter.getDisplay());
     }
+
+    protected abstract <T extends IsSerializable> GwtEvent.Type<LoadListEventHandler<T>> _listEvent();
 
     @Override protected void onUnbind() {
         super.onUnbind();
