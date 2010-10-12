ab05225f9b402aaca486122a5b00e60f1752078d
Michael Wolter <michael.wolter@atns.de>
2010-10-12 20:50:55 +0200

    LoadCreateEvent

---
+++ gwt-crud/src/main/java/de/atns/common/crud/client/event/LoadCreateEvent.java
@@ -0,0 +1,58 @@
+package de.atns.common.crud.client.event;
+
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.rpc.IsSerializable;
+import de.atns.common.gwt.client.ErrorWidgetDisplay;
+import de.atns.common.security.client.Callback;
+import de.atns.common.security.client.SharedServicesHolder;
+
+
+/**
+ * @author mwolter
+ * @since 22.09.2010 18:17:01
+ */
+public class LoadCreateEvent<E extends IsSerializable> extends GwtEvent<LoadCreateEventHandler<E>> {
+// ------------------------------ FIELDS ------------------------------
+
+    private final E result;
+    private Type<LoadCreateEventHandler<E>> type;
+    private final Object source;
+
+// -------------------------- STATIC METHODS --------------------------
+
+    public static <T extends IsSerializable> Callback<T> eventCallback(final ErrorWidgetDisplay display,
+                                                                       final Type<LoadCreateEventHandler<T>> type,
+                                                                       final Object source) {
+        return new Callback<T>(display) {
+            @Override public void callback(final T result) {
+                fireEvent(result, display, type, source);
+            }
+        };
+    }
+
+    public static <T extends IsSerializable> void fireEvent(final T result, final ErrorWidgetDisplay display,
+                                                            final Type<LoadCreateEventHandler<T>> type,
+                                                            final Object source) {
+        SharedServicesHolder.shared().getEventBus().fireEvent(new LoadCreateEvent<T>(result, type, source));
+        display.stopProcessing();
+    }
+
+// --------------------------- CONSTRUCTORS ---------------------------
+
+    public LoadCreateEvent(final E result, final Type<LoadCreateEventHandler<E>> type,
+                           final Object source) {
+        this.result = result;
+        this.type = type;
+        this.source = source;
+    }
+
+// -------------------------- OTHER METHODS --------------------------
+
+    @Override protected void dispatch(final LoadCreateEventHandler<E> handler) {
+        handler.onCreate(result, source);
+    }
+
+    @Override public Type<LoadCreateEventHandler<E>> getAssociatedType() {
+        return type;
+    }
+}
---
+++ gwt-crud/src/main/java/de/atns/common/crud/client/event/LoadCreateEventHandler.java
@@ -0,0 +1,14 @@
+package de.atns.common.crud.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * @author mwolter
+ * @since 22.09.2010 18:16:46
+ */
+public interface LoadCreateEventHandler<E extends IsSerializable> extends EventHandler {
+// -------------------------- OTHER METHODS --------------------------
+
+    void onCreate(E event, Object source);
+}
