590ca706224b06033c94e96ed2dafeb1e2c81f8e
Thomas Baum <thomas.baum@atns.de>
2010-11-23 13:15:46 +0100

    fix dialog-box widget presenter / showing

--- gwt/src/main/java/de/atns/common/gwt/client/DialogBoxPresenter.java
+++ gwt/src/main/java/de/atns/common/gwt/client/DialogBoxPresenter.java
@@ -1,23 +1,30 @@
 package de.atns.common.gwt.client;
 
-import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.user.client.ui.PopupPanel;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * @author tbaum
  * @since 07.12.2009
  */
 public abstract class DialogBoxPresenter<D extends DialogBoxDisplay> extends WidgetPresenter<D> {
+// ------------------------------ FIELDS ------------------------------
+
+    private final Logger LOG = Logger.getLogger(this.getClass().toString());
+
 // ------------------------ INTERFACE METHODS ------------------------
 
 
 // --------------------- Interface Activity ---------------------
 
     @Override public void start(AcceptsOneWidget panel, EventBus eventBus) {
+        LOG.log(Level.FINE, "Activity: start()");
         bind();
     }
 
@@ -34,11 +41,7 @@ public abstract class DialogBoxPresenter<D extends DialogBoxDisplay> extends Wid
             }
         });
 
-        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-            @Override public void execute() {
-                getDisplay().showDialogBox();
-            }
-        });
+        display.showDialogBox();
     }
 
     @Override
