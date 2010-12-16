4e3a0e008e5c4db685c5f41a8a770cb89dab0c20
Michael Wolter <michael.wolter@atns.de>
2010-12-16 14:21:02 +0100

    fix dialogbox-list presenter

--- gwt/src/main/java/de/atns/common/crud/client/DialogBoxListPresenter.java
+++ gwt/src/main/java/de/atns/common/crud/client/DialogBoxListPresenter.java
@@ -1,5 +1,6 @@
 package de.atns.common.crud.client;
 
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.client.rpc.IsSerializable;
@@ -18,20 +19,23 @@ public abstract class DialogBoxListPresenter<D extends DialogBoxDisplay & ListDi
         return super.getDisplay();
     }
 
-    @Override
-    protected void onBind() {
+    @Override protected void onBind() {
         super.onBind();
 
         display.addDialogBoxCloseCommand(new CloseHandler<PopupPanel>() {
-            @Override
-            public void onClose(final CloseEvent<PopupPanel> popupPanelCloseEvent) {
+            @Override public void onClose(final CloseEvent<PopupPanel> popupPanelCloseEvent) {
                 unbind();
+            }
+        });
+
+        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+            @Override public void execute() {
+                display.showDialogBox();
             }
         });
     }
 
-    @Override
-    public void unbind() {
+    @Override public void unbind() {
         super.unbind();
         if (display.isShowing()) {
             display.hideDialogBox();
