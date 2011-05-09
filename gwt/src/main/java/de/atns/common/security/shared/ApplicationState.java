f28b21fe25962c1062940a06b592278fc8b1b67e
Michael Wolter <michael.wolter@atns.de>
2011-05-09 10:43:27 +0200

    ApplicationState Checksessionaufruf bei initialisierung

--- gwt/src/main/java/de/atns/common/security/shared/ApplicationState.java
+++ gwt/src/main/java/de/atns/common/security/shared/ApplicationState.java
@@ -1,11 +1,14 @@
 package de.atns.common.security.shared;
 
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import de.atns.common.security.client.action.CheckSession;
 import de.atns.common.security.client.event.ServerStatusEvent;
 import de.atns.common.security.client.event.ServerStatusEventHandler;
 import de.atns.common.security.client.model.UserPresentation;
+import net.customware.gwt.dispatch.client.DispatchAsync;
 
 import static de.atns.common.security.client.event.ServerStatusEvent.ServerStatus.LOGGED_IN;
 import static de.atns.common.security.client.event.ServerStatusEvent.ServerStatus.LOGGED_OUT;
@@ -22,15 +25,27 @@ public class ApplicationState {
 
 // --------------------------- CONSTRUCTORS ---------------------------
 
-    @Inject public ApplicationState(final EventBus eventBus) {
+    @Inject
+    public ApplicationState(final EventBus eventBus, DispatchAsync dispatcher) {
         eventBus.addHandler(ServerStatusEventHandler.TYPE, new ServerStatusEventHandler() {
-            @Override public void onServerStatusChange(final ServerStatusEvent event) {
+            @Override
+            public void onServerStatusChange(final ServerStatusEvent event) {
                 final ServerStatusEvent.ServerStatus status = event.getStatus();
                 if (status == LOGGED_IN) {
                     user = event.getUser();
                 } else if (status == LOGGED_OUT) {
                     user = null;
                 }
+            }
+        });
+        dispatcher.execute(new CheckSession(), new AsyncCallback<UserPresentation>() {
+            @Override
+            public void onFailure(final Throwable caught) {
+            }
+
+            @Override
+            public void onSuccess(final UserPresentation user) {
+                ApplicationState.this.user = user;
             }
         });
     }
