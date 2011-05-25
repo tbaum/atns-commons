303bae8f4cfb5e4724e5a0b9fdef11e00d720076
Thomas Baum <thomas.baum@atns.de>
2011-05-25 14:14:17 +0200

    sender tasks

--- mail/src/main/java/de/atns/common/mail/EmailSenderTask.java
+++ mail/src/main/java/de/atns/common/mail/EmailSenderTask.java
@@ -3,18 +3,17 @@ package de.atns.common.mail;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import com.google.inject.persist.Transactional;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
+import javax.mail.event.TransportAdapter;
+import javax.mail.event.TransportEvent;
 import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Properties;
+import javax.persistence.FlushModeType;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -22,64 +21,148 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * @since 27.11.2009
  */
 @Singleton
-public class EmailSenderTask implements Runnable {
+public class EmailSenderTask extends TimerTask implements Runnable {
 // ------------------------------ FIELDS ------------------------------
 
     private static final Log LOG = LogFactory.getLog(EmailSenderTask.class);
-    private final Properties mailConfiguration;
-    private final EmailRepository repository;
+    private final Provider<Properties> mailConfiguration;
+    private final Provider<EmailRepository> repository;
     private final Provider<EntityManager> em;
     private final AtomicBoolean running = new AtomicBoolean(false);
 
 // --------------------------- CONSTRUCTORS ---------------------------
 
     @Inject public EmailSenderTask(final Provider<EntityManager> em,
-                                   @MailConfiguration final Properties mailConfiguration,
-                                   final EmailRepository repository) {
+                                   @MailConfiguration final Provider<Properties> mailConfiguration,
+                                   final Provider<EmailRepository> repository) {
         this.em = em;
         this.mailConfiguration = mailConfiguration;
         this.repository = repository;
     }
 
-// ------------------------ INTERFACE METHODS ------------------------
+// -------------------------- OTHER METHODS --------------------------
+
+    @Transactional
+    public void sendEmails() {
+        final Properties props1 = mailConfiguration.get();
+
+        String host = (String) props1.get("mail.smtp.host");
+        String user = (String) props1.get("mail.smtp.user");
+        String pass = (String) props1.get("mail.smtp.password");
+        boolean ssl = "true".equalsIgnoreCase((String) props1.get("mail.smtp.ssl"));
+        sendEmails(host, user, pass, ssl);
+    }
+
+    private void sendEmails(final String host, final String user, final String pass, final boolean ssl) {
+        /*
+        final EntityManager em = this.em.get();
+        final Session session = Session.getInstance(mailConfiguration);
+
+        final EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+
+            for (final EmailMessage p1 : repository.getAllUnsentMails()) {
+
+                final EmailMessage message = em.find(EmailMessage.class, p1.getId());
+                try {
+                    final MimeMessage mimeMessage = new MimeMessage(session);
+                    message.prepare(mimeMessage);
+                    LOG.debug("sending #" + p1.getId() + " " + p1.getSender() + " " + p1.getSubject() +
+                            " --> " + Arrays.toString(mimeMessage.getRecipients(Message.RecipientType.TO)));
+                    Transport.send(mimeMessage);
+                    message.setSent(new Date());
+
+                    em.merge(message);
+                } catch (Exception e) {
+                    LOG.error(e);
+                    message.setError(e.getMessage());
+                }
+                em.flush();
+            }
+            transaction.commit();
+        } finally {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+        }
+        */
 
 
-// --------------------- Interface Runnable ---------------------
+        final EntityManager em = this.em.get();
+        em.setFlushMode(FlushModeType.COMMIT);
+        final EmailRepository repository = this.repository.get();
+
+        final Session session;
+
+        Properties props = new Properties();
+        props.put("mail.smtp.dsn.notify", "SUCCESS,FAILURE,DELAY");
+        props.put("mail.smtp.dsn.ret", "HDRS");
+
+        if (host != null) {
+            props.put(ssl ? "mail.smtps.host" : "mail.smtp.host", host);
+        }
+        session = Session.getInstance(props);
+        Transport transport = null;
+        for (final EmailMessage me : repository.getAllUnsentMails()) {
+            EmailMessage message = em.find(EmailMessage.class, me.getId());
+            if (transport == null) {
+                transport = getTransport(session, ssl, host, user, pass);
+            }
+            try {
+                final MimeMessage mimeMessage = new MimeMessage(session);
+                message.prepare(mimeMessage);
+                LOG.debug("sending #" + message.getId() + " " + message.getSender() + " " + message.getSubject() +
+                        " --> " + Arrays.toString(mimeMessage.getRecipients(Message.RecipientType.TO)));
+
+                transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+                message.setSent(new Date());
+            } catch (Exception e) {
+                LOG.error(e);
+                message.setError(e.getMessage());
+            }
+            em.flush();
+        }
+    }
+
+    private Transport getTransport(final Session session, final boolean ssl, final String host, final String user, final String pass) {
+        try {
+            final Transport transport = session.getTransport(ssl ? "smtps" : "smtp");
+            transport.addTransportListener(new TransportAdapter() {
+                @Override public void messagePartiallyDelivered(final TransportEvent e) {
+                    LOG.debug("part " + e);
+                }
+
+                @Override public void messageNotDelivered(final TransportEvent e) {
+                    LOG.debug("not  " + e);
+                }
+
+                @Override public void messageDelivered(final TransportEvent e) {
+                    LOG.debug("del  " + e);
+                }
+            });
+            transport.connect(host, user, pass);
+
+            return transport;
+        } catch (NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void trigger() {
+        new Timer().schedule(new TimerTask() {
+                    @Override public void run() {
+                        EmailSenderTask.this.run();
+                    }
+                }, 500);
+    }
 
     @Override public void run() {
         if (running.compareAndSet(false, true)) {
             try {
-                final EntityManager em = this.em.get();
-                final Session session = Session.getInstance(mailConfiguration);
-
-                final EntityTransaction transaction = em.getTransaction();
-                try {
-                    transaction.begin();
-
-                    for (final EmailMessage p1 : repository.getAllUnsentMails()) {
-
-                        final EmailMessage message = em.find(EmailMessage.class, p1.getId());
-                        try {
-                            final MimeMessage mimeMessage = new MimeMessage(session);
-                            message.prepare(mimeMessage);
-                            LOG.debug("sending #" + p1.getId() + " " + p1.getSender() + " " + p1.getSubject() +
-                                    " --> " + Arrays.toString(mimeMessage.getRecipients(Message.RecipientType.TO)));
-                            Transport.send(mimeMessage);
-                            message.setSent(new Date());
-
-                            em.merge(message);
-                        } catch (Exception e) {
-                            LOG.error(e);
-                            message.setError(e.getMessage());
-                        }
-                        em.flush();
-                    }
-                    transaction.commit();
-                } finally {
-                    if (transaction.isActive()) {
-                        transaction.rollback();
-                    }
-                }
+                sendEmails();
             } catch (Exception e) {
                 LOG.error(e, e);
             } finally {
