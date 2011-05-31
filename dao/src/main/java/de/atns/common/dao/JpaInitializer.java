d60d93802cbb303eade9f2247f5a9ba5ae048d60
Michael Wolter <michael.wolter@atns.de>
2011-05-31 16:57:29 +0200

    commons logging jpa

--- dao/src/main/java/de/atns/common/dao/JpaInitializer.java
+++ dao/src/main/java/de/atns/common/dao/JpaInitializer.java
@@ -3,15 +3,26 @@ package de.atns.common.dao;
 
 import com.google.inject.Inject;
 import com.google.inject.persist.PersistService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * @author tbaum
  * @since 26.05.11 05:17
  */
 public class JpaInitializer {
+// ------------------------------ FIELDS ------------------------------
+
+    private final static Log LOG = LogFactory.getLog(JpaInitializer.class);
+
 // --------------------------- CONSTRUCTORS ---------------------------
 
     @Inject public JpaInitializer(PersistService persistService) {
-        persistService.start();
+        try {
+            persistService.start();
+        } catch (RuntimeException e) {
+            LOG.error(e, e);
+            throw e;
+        }
     }
 }
