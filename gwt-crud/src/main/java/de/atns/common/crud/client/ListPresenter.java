23bd8b7e92ecc79ceff4ccf453f304dc04fe048d
Michael Wolter <michael.wolter@atns.de>
2012-07-06 11:26:04 +0200

    auto update

--- gwt-crud/src/main/java/de/atns/common/crud/client/ListPresenter.java
+++ gwt-crud/src/main/java/de/atns/common/crud/client/ListPresenter.java
@@ -4,6 +4,8 @@ import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.IsSerializable;
 import com.google.inject.Inject;
 import de.atns.common.crud.client.event.LoadListEvent;
@@ -21,56 +23,22 @@ import net.customware.gwt.dispatch.shared.Action;
 public abstract class ListPresenter<D extends ListDisplay<T>, T extends IsSerializable> extends WidgetPresenter<D> {
 
     protected PagePresenter pagePresenter;
+    private Timer timer;
 
     @Inject public void setPagePresenter(final PagePresenter pagePresenter) {
         this.pagePresenter = pagePresenter;
     }
 
-    @Override protected void onBind() {
-        super.onBind();
+    protected void enableAutoupdate(int seconds, final Command command) {
+        timer = new Timer() {
 
-        pagePresenter.bind(this);
-        display.setPagePresenter(pagePresenter.getDisplay());
-
-        registerHandler(display.forSuche(new ClickHandler() {
-            @Override public void onClick(final ClickEvent event) {
-                pagePresenter.firstPage();
+            @Override public void run() {
                 updateList();
+                command.execute();
             }
-        }));
-
-        registerHandler(display.forPressEnter(new EnterKeyPressHandler() {
-            @Override protected void onEnterPressed() {
-                pagePresenter.firstPage();
-                updateList();
-            }
-        }));
-
-        registerHandler(eventBus.addHandler(_listEvent(),
-                new LoadListEventHandler<T>() {
-                    @Override public void onLoad(final ListPresentation<T> event, final Object source) {
-                        display.reset();
-                        if (event.getEntries() != null) {
-                            String lastValue = null;
-                            for (final T presentation : event.getEntries()) {
-                                lastValue = bindRow(presentation, lastValue);
-                            }
-                        }
-                    }
-                }));
-
-        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-            @Override public void execute() {
-                if (isBound()) {
-                    updateList();
-                }
-            }
-        });
+        };
+        timer.scheduleRepeating(seconds * 1000);
     }
-
-    protected abstract GwtEvent.Type<LoadListEventHandler<T>> _listEvent();
-
-    protected abstract String bindRow(T presentation, String lastValue);
 
     public void updateList() {
         final Action loadAction = createLoadAction();
@@ -83,22 +51,76 @@ public abstract class ListPresenter<D extends ListDisplay<T>, T extends IsSerial
 
     protected Callback<ListPresentation<T>> loadCallback() {
         return new Callback<ListPresentation<T>>(getErrorDisplay()) {
+
             @Override public void callback(final ListPresentation<T> result) {
                 LoadListEvent.fireEvent(result, _listEvent(), ListPresenter.this);
             }
         };
     }
 
-    @Override public D getDisplay() {
-        return super.getDisplay();
-    }
-
     public WidgetDisplay getErrorDisplay() {
         return super.getDisplay();
     }
 
+    protected abstract GwtEvent.Type<LoadListEventHandler<T>> _listEvent();
+
+    @Override public D getDisplay() {
+        return super.getDisplay();
+    }
+
+    @Override protected void onBind() {
+        super.onBind();
+
+        pagePresenter.bind(this);
+        display.setPagePresenter(pagePresenter.getDisplay());
+
+        registerHandler(display.forSuche(new ClickHandler() {
+
+            @Override public void onClick(final ClickEvent event) {
+                pagePresenter.firstPage();
+                updateList();
+            }
+        }));
+
+        registerHandler(display.forPressEnter(new EnterKeyPressHandler() {
+
+            @Override protected void onEnterPressed() {
+                pagePresenter.firstPage();
+                updateList();
+            }
+        }));
+
+        registerHandler(eventBus.addHandler(_listEvent(),
+                new LoadListEventHandler<T>() {
+
+                    @Override public void onLoad(final ListPresentation<T> event, final Object source) {
+                        display.reset();
+                        if (event.getEntries() != null) {
+                            String lastValue = null;
+                            for (final T presentation : event.getEntries()) {
+                                lastValue = bindRow(presentation, lastValue);
+                            }
+                        }
+                    }
+                }));
+
+        Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+
+            @Override public void execute() {
+                if (isBound()) {
+                    updateList();
+                }
+            }
+        });
+    }
+
+    protected abstract String bindRow(T presentation, String lastValue);
+
     @Override protected void onUnbind() {
         super.onUnbind();
         pagePresenter.unbind();
+        if (timer != null) {
+            timer.cancel();
+        }
     }
 }
